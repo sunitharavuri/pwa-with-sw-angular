e0e9372c9327ec3e790810b835169ef2b38fac63
sunitharavuri <sravuri@nisum.com>
2017-09-20 18:35:48 -0700

    pwa-demo app with service worker

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { BeerListComponent } from './beer-list/beer-list.component';
+import { BeerService } from './shared/beer/beer.service';
+import { GiphyService } from './shared/giphy/giphy.service';
+import { MaterialModule } from '@angular/material';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BeerListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule
+  ],
+  providers: [BeerService, GiphyService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
