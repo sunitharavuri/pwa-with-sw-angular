9f599aee88082ab20a8899be224135bae11f9aef
sunitharavuri <sravuri@nisum.com>
2017-09-20 19:08:52 -0700

    fixing all

---
+++ pwademoproject/src/sw.js
@@ -0,0 +1,11 @@
+// self.addEventListener('install', (e) => {
+//     console.log('Service Worker: Installed');
+// });
+
+// self.addEventListener('activate', (e) => {
+//     console.log('Service Worker: Active');
+// });
+
+// self.addEventListener('fetch', (e) => {
+//     console.log('Service Worker: Fetch');
+// });
